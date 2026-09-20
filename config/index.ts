120ef5043d613e4f1b6306de016cdd17d79b2d0d
prncssminer-crypto <prncssminer@gmail.com>
2026-09-20 03:08:35 +0000

    description

--- config/index.ts
+++ config/index.ts
@@ -5,7 +5,7 @@ export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
   title: 'Agent: Market Research',
-  description: 'App description',
+  description: 'test 1',
   copyright: '',
   privacy_policy: '',
   default_language: 'en',
