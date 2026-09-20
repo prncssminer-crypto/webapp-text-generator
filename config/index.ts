4f0b2e323e475a93de724250519613fc74014d3e
prncssminer-crypto <prncssminer@gmail.com>
2026-09-20 02:52:48 +0000

    en

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,7 @@ export const APP_INFO: AppInfo = {
   description: 'App description',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en-US',
+  default_language: 'en',
 }
 
 export const API_PREFIX = `${process.env.NEXT_PUBLIC_API_PREFIX || '/api'}`
